817f873a68f7cfa6e3528fdaca5eabac17c27052
Elson Li <elson.li@berkeley.edu>
2020-01-06 15:24:08 -0800

    5 Artwork seeds, initialize ArtworksController

--- app/models/artwork.rb
+++ app/models/artwork.rb
@@ -1,11 +1,12 @@
 class Artwork < ApplicationRecord
 
   validates :title, presence: true
+  validates :video, 
 
   # has_many :likes
   # has_many :comments
-  # has_one_attached :video
-  # has_many_attached :photos
+  has_one_attached :video, service: :s3
+  # has_many_attached :photos, service: :s3
   
   # https://blog.capsens.eu/how-to-use-activestorage-in-your-rails-5-2-application-cdf3a3ad8d7
 
