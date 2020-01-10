e72217f1421988ff75206840eafd95a2f1289e7e
Elson Li <elson.li@berkeley.edu>
2020-01-10 13:48:39 -0800

    Darker modal background for artworks

--- frontend/components/modal/artwork_modal.jsx
+++ frontend/components/modal/artwork_modal.jsx
@@ -7,7 +7,9 @@ class ArtworkModal extends React.Component {
 
   componentDidMount() {
     document.getElementsByClassName("session-modal-child")[0].classList.add("artwork-modal-child");
+    document.getElementsByClassName("session-modal-background")[0].classList.add("artwork-modal-background");
     document.getElementsByClassName("session-modal-child")[0].classList.remove("session-modal-child");
+    document.getElementsByClassName("session-modal-background")[0].classList.remove("session-modal-background");
   };
 
   render() {
