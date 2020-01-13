6513a6c0727f498cd609ce69fb21b5d7ed5cf9c9
Elson Li <elson.li@berkeley.edu>
2020-01-13 11:33:04 -0800

    heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -47,42 +47,42 @@ artwork_1.videos.attach(io: open("https://www.youtube.com/watch?v=iUSXVxOb4Hk"),
 #   description: "Anonymous and diverse figures walk freely in space. They sing, and when they walk near visitors, they sing more. Just as the figures in the artwork move freely singing and dancing in any direction, people in real life are free to dance anywhere and walk in any direction.\n\nThis work is to be viewed from a position close to the entrance.\n\nThe work begins when the figures of Walk, Walk, Walk enter into the Cave Universe in the middle of the aisle. When the work ends, when the figures leave the space and disappear.\n\nIf you stand at position close to the entrance the boundary between the wall and the floor disappears, the real space dissolves and the body becomes immersed in the artwork.\n\nThe work is rendered in real time by a computer program, it is neither a prerecorded animation nor imagery on loop. Previous states will never be repeated and can never be seen again."
 # )
 
-artwork_2.photos.attach(io: open("https://borderless.teamlab.art/images/pc-m/14910")), filename: "Image_01.jpeg")
+artwork_2.photos.attach(io: open("https://borderless.teamlab.art/images/pc-m/14910"), filename: "Image_01.jpeg")
 
 # artwork_3 = Artwork.create!(
 #   title: "The Way of the Sea, Floating Nest",
 #   description: "A large nest floats in the center of the space. The work is viewed by lying down or sitting in the Floating Nest.\n\nThis work starts when the shoal of fish from The Way of the Sea passes through the Flower Forest and enters the Floating Nest. The work ends when the fish leave the space and disappear.\n\nLie down in the Floating Nest and the lines drawn by the traces of the fish appears to be drawn in three dimensions in the space. The body becomes immersed into the work, and the boundary between people and the work dissolves.\n\nThe fish are rendered in light as they fly around the space, leaving trails of light in their paths and creating spatial calligraphy.\n\n'The way of the bird' is a saying in Zen which refers to the fact that unlike a human road the road of the bird is not fixed. Its meaning is freedom and of leaving no trace. Here the birds are replaced with fish.\n\nThe movement of thousands of fish and birds is beautiful and mysterious, like a single giant life form. The flock has neither a leader nor mutual consensus, but moves on the simple basis of; if my neighbor moves, then I move too. The biological mechanism that causes flocks of hundreds of fish and birds to move at the same time is enigmatic. It seems there is a universal principle that humans do not understand. Likewise, the coloring of the flock is not predetermined. Influenced by the presence of people, the space becomes a complex and beautiful coloration. This coloration is created by the movement of each fish and bird obeying some kind of primitive rule.\n\nThe work is rendered in real time by a computer program, it is neither a prerecorded animation nor imagery on loop. The work as a whole is in constant change; previous states will never be repeated and can never be seen again."
 # )
 
-artwork_3.photos.attach(io: open("https://borderless.teamlab.art/images/pc-m/14882")), filename: "Image_01.jpeg")
-artwork_3.photos.attach(io: open("https://borderless.teamlab.art/images/pc-m/14883")), filename: "Image_02.jpeg")
-artwork_3.photos.attach(io: open("https://borderless.teamlab.art/images/pc-m/16195")), filename: "Image_03.jpeg")
+artwork_3.photos.attach(io: open("https://borderless.teamlab.art/images/pc-m/14882"), filename: "Image_01.jpeg")
+artwork_3.photos.attach(io: open("https://borderless.teamlab.art/images/pc-m/14883"), filename: "Image_02.jpeg")
+artwork_3.photos.attach(io: open("https://borderless.teamlab.art/images/pc-m/16195"), filename: "Image_03.jpeg")
 
 artwork_4 = Artwork.create!(
   title: "Wander through the Crystal World",
   description: "This interactive installation artwork uses an accumulation of light points to create a sculptural body, similar to the way distinct dots of color form an image in a pointillist painting. The accumulation of lights in three-dimensional space form an expression of a world inside the Crystal World.\n\nBack when kanji (hieroglyphic characters) were carved into the shell of a turtle, the bone of a cow or deer, or cast in a piece of bronze ware, each character contained an entire world within in. Here, a Crystal World is created when a person summons the worlds contained in the characters up, through their smartphone. Each summoned world has an affect on the world of other visitors', and is influenced by the people in the installation.\n\nButterflies drawn and created by visitors at other A Lull In The Museum locations, or through other works of art, can make their way into this world, transformed into 3D creations through points of light.\n\nDue to visitor interaction this Crystal World is in perpetual change, the space is reborn moment by moment."
 )
 
-artwork_4.photos.attach(io: open("https://borderless.teamlab.art/images/pc-m/16227")), filename: "Image_01.jpeg")
-artwork_4.photos.attach(io: open("https://borderless.teamlab.art/images/pc-m/16221")), filename: "Image_02.jpeg")
-artwork_4.photos.attach(io: open("https://borderless.teamlab.art/images/pc-m/14928")), filename: "Image_03.jpeg")
-artwork_4.photos.attach(io: open("https://borderless.teamlab.art/images/pc-m/15039")), filename: "Image_04.jpeg")
-artwork_4.photos.attach(io: open("https://borderless.teamlab.art/images/pc-m/15119")), filename: "Image_05.jpeg")
-artwork_4.photos.attach(io: open("https://borderless.teamlab.art/images/pc-m/15120")), filename: "Image_06.jpeg")
-artwork_4.photos.attach(io: open("https://borderless.teamlab.art/images/pc-m/15121")), filename: "Image_07.jpeg")
-artwork_4.photos.attach(io: open("https://borderless.teamlab.art/images/pc-m/16216")), filename: "Image_08.jpeg")
-artwork_4.photos.attach(io: open("https://borderless.teamlab.art/images/pc-m/16217")), filename: "Image_09.jpeg")
-artwork_4.photos.attach(io: open("https://borderless.teamlab.art/images/pc-m/16218")), filename: "Image_10.jpeg")
-artwork_4.photos.attach(io: open("https://borderless.teamlab.art/images/pc-m/16219")), filename: "Image_11.jpeg")
-artwork_4.photos.attach(io: open("https://borderless.teamlab.art/images/pc-m/16220")), filename: "Image_12.jpeg")
-artwork_4.photos.attach(io: open("https://borderless.teamlab.art/images/pc-m/16222")), filename: "Image_13.jpeg")
-artwork_4.photos.attach(io: open("https://borderless.teamlab.art/images/pc-m/16223")), filename: "Image_14.jpeg")
-artwork_4.photos.attach(io: open("https://borderless.teamlab.art/images/pc-m/16224")), filename: "Image_15.jpeg")
-artwork_4.photos.attach(io: open("https://borderless.teamlab.art/images/pc-m/16225")), filename: "Image_16.jpeg")
-artwork_4.photos.attach(io: open("https://borderless.teamlab.art/images/pc-m/16226")), filename: "Image_17.jpeg")
-artwork_4.photos.attach(io: open("https://borderless.teamlab.art/images/pc-m/16228")), filename: "Image_18.jpeg")
-artwork_4.photos.attach(io: open("https://borderless.teamlab.art/images/pc-m/16229")), filename: "Image_19.jpeg")
-artwork_4.photos.attach(io: open("https://borderless.teamlab.art/images/pc-m/16230")), filename: "Image_20.jpeg")
+artwork_4.photos.attach(io: open("https://borderless.teamlab.art/images/pc-m/16227"), filename: "Image_01.jpeg")
+artwork_4.photos.attach(io: open("https://borderless.teamlab.art/images/pc-m/16221"), filename: "Image_02.jpeg")
+artwork_4.photos.attach(io: open("https://borderless.teamlab.art/images/pc-m/14928"), filename: "Image_03.jpeg")
+artwork_4.photos.attach(io: open("https://borderless.teamlab.art/images/pc-m/15039"), filename: "Image_04.jpeg")
+artwork_4.photos.attach(io: open("https://borderless.teamlab.art/images/pc-m/15119"), filename: "Image_05.jpeg")
+artwork_4.photos.attach(io: open("https://borderless.teamlab.art/images/pc-m/15120"), filename: "Image_06.jpeg")
+artwork_4.photos.attach(io: open("https://borderless.teamlab.art/images/pc-m/15121"), filename: "Image_07.jpeg")
+artwork_4.photos.attach(io: open("https://borderless.teamlab.art/images/pc-m/16216"), filename: "Image_08.jpeg")
+artwork_4.photos.attach(io: open("https://borderless.teamlab.art/images/pc-m/16217"), filename: "Image_09.jpeg")
+artwork_4.photos.attach(io: open("https://borderless.teamlab.art/images/pc-m/16218"), filename: "Image_10.jpeg")
+artwork_4.photos.attach(io: open("https://borderless.teamlab.art/images/pc-m/16219"), filename: "Image_11.jpeg")
+artwork_4.photos.attach(io: open("https://borderless.teamlab.art/images/pc-m/16220"), filename: "Image_12.jpeg")
+artwork_4.photos.attach(io: open("https://borderless.teamlab.art/images/pc-m/16222"), filename: "Image_13.jpeg")
+artwork_4.photos.attach(io: open("https://borderless.teamlab.art/images/pc-m/16223"), filename: "Image_14.jpeg")
+artwork_4.photos.attach(io: open("https://borderless.teamlab.art/images/pc-m/16224"), filename: "Image_15.jpeg")
+artwork_4.photos.attach(io: open("https://borderless.teamlab.art/images/pc-m/16225"), filename: "Image_16.jpeg")
+artwork_4.photos.attach(io: open("https://borderless.teamlab.art/images/pc-m/16226"), filename: "Image_17.jpeg")
+artwork_4.photos.attach(io: open("https://borderless.teamlab.art/images/pc-m/16228"), filename: "Image_18.jpeg")
+artwork_4.photos.attach(io: open("https://borderless.teamlab.art/images/pc-m/16229"), filename: "Image_19.jpeg")
+artwork_4.photos.attach(io: open("https://borderless.teamlab.art/images/pc-m/16230"), filename: "Image_20.jpeg")
 
 # artwork_5 = Artwork.create!(
 #   title: "Memory of Topography",
