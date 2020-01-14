385b8a3cb262c9b7fb36b7ed37b4d3c89fcd9d55
Elson Li <elson.li@berkeley.edu>
2020-01-13 23:25:03 -0800

    Remove things from window

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -2,8 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Root from "./components/root";
 import configureStore from "./store/store";
-import * as SessionActions from "./actions/session_actions";
-import * as CommentActions from "./actions/comment_actions";
 
 document.addEventListener("DOMContentLoaded", () => {
   let store;
@@ -19,16 +17,6 @@ document.addEventListener("DOMContentLoaded", () => {
   } else {
     store = configureStore();
   }
-  
-  // Window Testing Start
-  window.userSignup = SessionActions.userSignup;
-  window.userLogin = SessionActions.userLogin;
-  window.userLogout = SessionActions.userLogout;
-  window.fetchComments = CommentActions.fetchComments;
-  window.fetchUser = SessionActions.fetchUser;
-  window.getState = store.getState;
-  window.dispatch = store.dispatch;
-  // Window Testing End
   
   const root = document.getElementById("root");
   ReactDOM.render(<Root store={ store } />, root);
