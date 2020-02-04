8828b5ad8b139d86146fd5ce27dc1ebef772c58e
Elson Li <elson.li@berkeley.edu>
2020-02-04 13:10:53 -0800

    Remove destroyLike from user dash

--- frontend/components/artwork/artwork_dash_item.jsx
+++ frontend/components/artwork/artwork_dash_item.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import * as Selectors from "../../reducers/selector";
-import { destroyLike } from "../../util/like_api_util";
 
 const ArtworkDashItem = props => {
   const {
@@ -21,27 +20,27 @@ const ArtworkDashItem = props => {
   ) : null;
 
   // Selects all of the likes by the current user
-  const selectedLikes = Selectors.likesByUser(likes, currentUserId);
+  // const selectedLikes = Selectors.likesByUser(likes, currentUserId);
 
   // Contains all of the artwork IDs liked by the current user
-  const selectedLikesSet = new Set(selectedLikes.map(like => like.artworkId));
+  // const selectedLikesSet = new Set(selectedLikes.map(like => like.artworkId));
 
   // Retrieves the specific like of the artwork by the current user
-  const likeObj = selectedLikes.filter(like => like.artworkId === artwork.id)[0];
+  // const likeObj = selectedLikes.filter(like => like.artworkId === artwork.id)[0];
 
   // Shows a clickable icon based on whether the artwork has been liked already
-  const liked = currentUserId ? (
-    selectedLikesSet.has(artwork.id) ? (
-      <div className="fa">
-        <i className="fas fa-star" onClick={ () => destroyLike(likeObj.id) }></i>
-      </div>
-    ) : null
-  ) : null;
+  // const liked = currentUserId ? (
+  //   selectedLikesSet.has(artwork.id) ? (
+  //     <div className="fa">
+  //       <i className="fas fa-star" onClick={ () => destroyLike(likeObj.id) }></i>
+  //     </div>
+  //   ) : null
+  // ) : null;
 
   return (
     // Previews contain a title and its first image that opens a new tab on click
     <li className={ `artworks-dash-${ artwork.id } contain-zoom-dash dash-item` } key={ artwork.id } >
-      { liked }
+      {/* { liked } */}
       <Link to={ `/artworks/${artwork.id}` } target="_blank">
         <h6 className="artworks-title">{ artwork.title }</h6>
       </Link>
